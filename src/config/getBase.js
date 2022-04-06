9a39784f867902b9cb48516a6fee2e936e536b83
Rasoul <rasoulkordestani12@gmail.com>
2022-04-06 09:21:00 +0430

    fix: baseUrl being "" always

--- src/config/getBase.js
+++ src/config/getBase.js
@@ -1,7 +1,8 @@
 export default function getBaseUrl() {
   // return process.env.REACT_APP_DEVELOPMENT_BASE_URL;
-  if (window.location.href.includes("localhost")) {
-    return "";
-  }
-  return process.env.REACT_APP_BASE_URL;
+  // if (window.location.href.includes("localhost")) {
+  //   return "";
+  // }
+  // return process.env.REACT_APP_BASE_URL;
+  return "";
 }
