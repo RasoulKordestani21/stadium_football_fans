961bb0a813e4926ea4ba3a8a831b85bab2048709
Rasoul <rasoulkordestani12@gmail.com>
2022-07-31 23:03:00 +0430

    feat: add background ( such wave shape )

---
+++ src/components/layoutBg/layoutBackground.js
@@ -0,0 +1,26 @@
+const LayoutBackground = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1094.848"
+      height="654.224"
+      viewBox="0 0 1094.848 654.224"
+      style={{
+        position: "fixed",
+        zIndex: "-1",
+        bottom: "-158px",
+        left: "-434px"
+      }}
+    >
+      <path
+        d="M995.378 875.185v314.376l-1007.231 63.063L-28 840.691c146.578 87.356 255.659 85.992 331.38 65.246 81.02-22.207 139.038-70.773 204.844-93.053 36.78-12.443 75.987-16.679 122.991-3.5a232.12 232.12 0 0 1 36.275 13.481c53.747 25.224 82.145 62.723 156.883 75.83 64.494 11.321 123.053-2.63 171.005-23.51z"
+        transform="rotate(12.04 3843.64 977.226)"
+        fill="#5f53ea"
+        fill-rule="evenodd"
+        opacity=".13"
+      />
+    </svg>
+  );
+};
+
+export default LayoutBackground;
