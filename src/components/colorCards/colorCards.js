9771f7152c7ef3853115873ff4c7d7c08eba4527
Rasoul <rasoulkordestani12@gmail.com>
2022-09-02 21:06:11 +0430

    feat: handle color code for fans

--- src/components/colorCards/colorCards.js
+++ src/components/colorCards/colorCards.js
@@ -1,15 +1,23 @@
+import React from "react";
 import clsx from "clsx";
 import { baseColors } from "../../config/colors";
+import { useRef } from "react";
+
+import classes from "./colorCards.module.css";
 
 const ColorCardsComp = ({ data, className, ...props }) => {
+  const [anmiation, setAnimation] = React.useState("fade-animation");
+  setTimeout(() => {
+    setAnimation("");
+  }, 11000);
   return (
     <div
       style={props.style}
       className={clsx(
-        `w-[180px] h-[270px] absolute  rounded-[6px] flex  text-white `
+        `w-[180px] h-[270px] absolute  rounded-[6px] flex  text-white shadow-md border-[1px] border-solid border-black `,
+        classes["fade-animation"]
       )}
     >
-      {console.log(data)}
       <div
         className={clsx(`w-[140px] h-[230px]  border-l-stone-300
        border-solid border-[2px] rounded-[6px] m-auto flex flex-col 
--- src/pages/main/colorsPage.js
+++ src/pages/main/colorsPage.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import ColorCardsComp from "../../components/colorCards/colorCards";
 import MainLayout from "../../components/layout/mainLayout";
 import clsx from "clsx";
@@ -13,10 +14,25 @@ import { baseColors } from "../../config/colors";
 
 const ColorsPage = props => {
   const imageId = useLocation().state.imageId;
+  const [cardId, setCardId] = React.useState();
   const { data } = useQuery("image-data", () =>
     instance.get("/api/peronData/scanQrCode")
   );
+  const [singleCard, setSingleCard] = React.useState(true);
 
+  setTimeout(() => {
+    setSingleCard(false);
+  }, 8000);
+
+  useEffect(() => {
+    if (data)
+      setCardId(
+        +findPixelColorId(
+          convertTo1DArray(JSON.parse(data?.data?.image)),
+          +imageId
+        ) + 1
+      );
+  }, [data, imageId]);
   return (
     <MainLayout colorsPage={true}>
       {console.log(
@@ -37,37 +53,57 @@ const ColorsPage = props => {
             "w-[80%] relative flex h-[100%] justify-center items-center"
           )}
         >
-          {baseColors.map((ele, index) => {
-            console.log(index);
-            return (
-              <ColorCardsComp
-                style={{
-                  backgroundColor: ele.colorCode,
-                  zIndex: `${index}`,
-                  left: `${index * 20 - 20}px`,
-                  color: ele.id === 9 ? "#000000" : "#ffffff"
-                }}
-                data={{
-                  name: ele.name,
-                  number: index + 1
-                }}
-                // data={{
-                //   cardColor: "bg-" + ele.name
-                //   // index: "z-[" + ele.id + "]"
-                //   // zIndex: "z-[" + ele.id + "]"
-                // }}
-              />
-            );
-          })}
+          {singleCard ? (
+            baseColors.map((ele, index) => {
+              return (
+                <ColorCardsComp
+                  style={{
+                    backgroundColor: ele.colorCode,
+                    zIndex: `${index}`,
+                    right: `${index * 18 - 30}px`,
+                    color: ele.id === 9 ? "#000000" : "#ffffff"
+                  }}
+                  data={{
+                    name: ele.namePe,
+                    number: index + 1
+                  }}
+                  // data={{
+                  //   cardColor: "bg-" + ele.name
+                  //   // index: "z-[" + ele.id + "]"
+                  //   // zIndex: "z-[" + ele.id + "]"
+                  // }}
+                />
+              );
+            })
+          ) : (
+            <ColorCardsComp
+              style={{
+                backgroundColor: baseColors[cardId].colorCode,
+                zIndex: +imageId,
+                right: `${55}px`,
+                color: cardId === 9 ? "#000000" : "#ffffff"
+              }}
+              data={{
+                name: baseColors[cardId].namePe,
+                number: cardId
+              }}
+              // data={{
+              //   cardColor: "bg-" + ele.name
+              //   // index: "z-[" + ele.id + "]"
+              //   // zIndex: "z-[" + ele.id + "]"
+              // }}
+            />
+          )}
         </div>
       </div>
       <div>
         <h3 className={clsx("mb-5")}>کارت شما کارت شماره 3 به رنگ آبی</h3>
         <p className={clsx("mb-2")}>کاربر عزیز</p>
         <p className={clsx("w-[80%] mx-auto")}>
-          کارتی که تو باید بالا ببری کارت شماره 3 به رنگ آبی است. بعد از شنیدن
-          اعلام شروع به مدت 1 دقیقه کارت رو بالای سرت ببر.میخواهیم تو بازه دقایق
-          20 تا 30 حریف رو میخکوب کنیم منتظر باش
+          کارتی که تو باید بالا ببری کارت شماره {cardId} به رنگ{" "}
+          {cardId && baseColors[cardId].namePe} است. بعد از شنیدن اعلام شروع به
+          مدت 1 دقیقه کارت رو بالای سرت ببر.میخواهیم تو بازه دقایق 20 تا 30 حریف
+          رو میخکوب کنیم منتظر باش
         </p>
       </div>
       <BottomNavigation />
